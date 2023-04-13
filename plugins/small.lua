214fbe98215f082fac3df7ffd42f975a27a1fff2
Ejiy <justlazzy.development@proton.me>
2023-04-13 10:49:14 +0900

    small

---
+++ plugins/small.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = { "User AstroFile" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+  },
+}
